1d14ba133f1431de5409eefdb08523c8d24e19cf
Simone Martucci <lightsaber_91@hotmail.it>
2015-08-02 11:18:52 +0200

    aggiunto segnale stampa autocorrelazione

--- user_signal.c
+++ user_signal.c
@@ -1,10 +1,18 @@
 #include <signal.h>
 
 void sigprint() {
+    clear_console();
     print_system_state(SYSTEM_PRINT);
+}
+
+void sigauto() {
+    clear_console();
+    print_system_state(SYSTEM_PRINT);
+    compute_autocorrelation();
+    exit(EXIT_SUCCESS);
 }
 
 void add_signals() {
     signal(SIGUSR1, sigprint);
-    signal(SIGUSR2, sigprint);
+    signal(SIGUSR2, sigauto);
 }
